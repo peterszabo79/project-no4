01a9dd650ca5c72974da982ffc5208639324dcea
peterszabo79 <peterszabo79@gmail.com>
2022-08-14 15:57:57 +0000

    deployment commit

--- sholiprj/settings.py
+++ sholiprj/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['shoppinglistbypsz.herokuapp.com' 'localhost']
+ALLOWED_HOSTS = ['shoppinglistbypsz.herokuapp.com', 'localhost']
 
 
 # Application definition
